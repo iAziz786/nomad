ab0bd660e3a0a9eeface3a967e09e761f904e713
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-11-10 14:16:32 -0500

    Homepage updates

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -185,23 +185,6 @@ export default function Homepage() {
           },
           {
             quote:
-              'We have people who are first-time system administrators deploying applications. There is a guy on our team who worked in IT help desk for 8 years - just today he upgraded an entire cluster himself.',
-            caseStudyURL: 'https://www.hashicorp.com/case-studies/roblox/',
-            person: {
-              firstName: 'Rob',
-              lastName: 'Cameron',
-              photo:
-                'https://www.datocms-assets.com/2885/1582180216-rob-cameron.jpeg',
-              title: 'Technical Director of Infrastructure',
-            },
-            company: {
-              name: 'Roblox',
-              logo:
-                'https://www.datocms-assets.com/2885/1582180369-roblox-color.svg',
-            },
-          },
-          {
-            quote:
               'Our customers’ jobs are changing constantly. It’s challenging to dynamically predict demand, what types of jobs, and the resource requirements. We found that Nomad excelled in this area.',
             caseStudyURL:
               'https://www.hashicorp.com/resources/nomad-vault-circleci-security-scheduling',
@@ -244,7 +227,8 @@ export default function Homepage() {
             person: {
               firstName: 'Dhasharath',
               lastName: 'Shrivathsa',
-              photo: 'https://www.datocms-assets.com/2885/1623450501-dhasharath-shrivathsa.jpg',
+              photo:
+                'https://www.datocms-assets.com/2885/1623450501-dhasharath-shrivathsa.jpg',
               title: 'CEO',
             },
             company: {
@@ -277,11 +261,6 @@ export default function Homepage() {
             companyName: 'Trivago',
             url:
               'https://www.datocms-assets.com/2885/1582162317-trivago-monochromatic.svg',
-          },
-          {
-            companyName: 'Roblox',
-            url:
-              'https://www.datocms-assets.com/2885/1582180373-roblox-monochrome.svg',
           },
           {
             companyName: 'CircleCI',
